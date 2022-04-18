d9ea6bfa3c60563bcdd7d1b1dd31a45de7ff6b5d
Johann Dirry <movgp0@gmail.com>
2022-04-18 19:08:23 +0200

    implementing LeviCivita tensor

---
+++ Femtools.UnitTests/LeviCivitaTests.cs
@@ -0,0 +1,74 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using NUnit.Framework;
+
+namespace Femtools.UnitTests;
+
+[TestOf(typeof(LeviCivita))]
+public sealed class LeviCivitaTests
+{
+    [TestOf(nameof(LeviCivita.OfRank))]
+    public sealed class OfRankTests
+    {
+        [Test]
+        public void Rank2()
+        {
+            var ε2 = LeviCivita.OfRank(2);
+
+            using var scope = new AssertionScope();
+            ((long)ε2[0, 0]).Should().Be(+0L, "[0,0] has repeating index");
+            ((long)ε2[0, 1]).Should().Be(+1L, "[0,1] is odd permutation");
+            ((long)ε2[1, 0]).Should().Be(-1L, "[1,0] is even permutation");
+            ((long)ε2[1, 1]).Should().Be(+0L, "[1,1] has repeating index");
+        }
+
+        [Test]
+        public void Rank3()
+        {
+            var ε3 = LeviCivita.OfRank(3);
+
+            using var scope = new AssertionScope();
+            ((long)ε3[0, 0, 0]).Should().Be(+0L, "[0,0,0] has repeating index");
+            ((long)ε3[0, 0, 1]).Should().Be(+0L, "[0,0,1] has repeating index");
+            ((long)ε3[0, 0, 2]).Should().Be(+0L, "[0,0,2] has repeating index");
+            ((long)ε3[0, 1, 0]).Should().Be(+0L, "[0,1,0] has repeating index");
+            ((long)ε3[0, 1, 1]).Should().Be(+0L, "[0,1,1] has repeating index");
+            ((long)ε3[0, 1, 2]).Should().Be(+1L, "[0,1,2] is even permutation");
+            ((long)ε3[0, 2, 0]).Should().Be(+0L, "[0,2,0] has repeating index");
+            ((long)ε3[0, 2, 1]).Should().Be(-1L, "[0,2,1] is odd permutation");
+            ((long)ε3[0, 2, 2]).Should().Be(+0L, "[0,2,2] has repeating index");
+
+            ((long)ε3[1, 0, 0]).Should().Be(+0L, "[1,0,0] has repeating index");
+            ((long)ε3[1, 0, 1]).Should().Be(+0L, "[1,0,1] has repeating index");
+            ((long)ε3[1, 0, 2]).Should().Be(-1L, "[1,0,2] is odd permutation");
+            ((long)ε3[1, 1, 0]).Should().Be(+0L, "[1,1,0] has repeating index");
+            ((long)ε3[1, 1, 1]).Should().Be(+0L, "[1,1,1] has repeating index");
+            ((long)ε3[1, 1, 2]).Should().Be(+0L, "[1,1,2] has repeating index");
+            ((long)ε3[1, 2, 0]).Should().Be(+1L, "[1,2,0] is even permutation");
+            ((long)ε3[1, 2, 1]).Should().Be(+0L, "[1,2,1] has repeating index");
+            ((long)ε3[1, 2, 2]).Should().Be(+0L, "[1,2,2] has repeating index");
+
+            ((long)ε3[2, 0, 0]).Should().Be(+0L, "[2,0,0] has repeating index");
+            ((long)ε3[2, 0, 1]).Should().Be(+1L, "[2,0,1] is even permutation");
+            ((long)ε3[2, 0, 2]).Should().Be(+0L, "[2,0,2] has repeating index");
+            ((long)ε3[2, 1, 0]).Should().Be(-1L, "[2,1,0] is odd permutation");
+            ((long)ε3[2, 1, 1]).Should().Be(+0L, "[2,1,1] has repeating index");
+            ((long)ε3[2, 1, 2]).Should().Be(+0L, "[2,1,2] has repeating index");
+            ((long)ε3[2, 2, 0]).Should().Be(+0L, "[2,2,0] has repeating index");
+            ((long)ε3[2, 2, 1]).Should().Be(+0L, "[2,2,1] has repeating index");
+            ((long)ε3[2, 2, 2]).Should().Be(+0L, "[2,2,2] has repeating index");
+        }
+
+        [Test]
+        public void Rank4()
+        {
+            var ε4 = LeviCivita.OfRank(4);
+
+            using var scope = new AssertionScope();
+            ((long)ε4[0, 3, 2, 1]).Should().Be(-1L, "[0,3,2,1] is odd permutation");
+            ((long)ε4[1, 0, 2, 3]).Should().Be(-1L, "[1,0,2,3] is odd permutation");
+            ((long)ε4[3, 2, 1, 0]).Should().Be(+1L, "[3,2,1,0] is even permutation");
+            ((long)ε4[2, 1, 3, 2]).Should().Be(+0L, "[2,1,3,2] has repeating index");
+        }
+    }
+}
---
+++ Femtools/LeviCivita.cs
@@ -0,0 +1,73 @@
+﻿using static TorchSharp.torch;
+
+namespace Femtools;
+
+public static class LeviCivita
+{
+    public static Tensor OfRank(int dimension)
+    {
+        // calculate the sizes for the tensor
+        var size = new long[dimension];
+        for (var i = 0L; i < dimension; i++)
+            size[i] = dimension;
+
+        // create the tensor
+        var t = zeros(size, dtype: ScalarType.Int64);
+
+        // create list of numbers [0..n] that get permuted for the position
+        var a = new long[dimension];
+        for (var i = 0L; i < dimension; i++)
+        {
+            a[i] = i;
+        }
+
+        // permute over all positions of the tensor
+        var permutations = CombinationsWithRepetition(a, a.Length);
+        foreach (var perm in permutations)
+        {
+            var pos = perm.ToArray();
+            var idx = pos.Select(p => (TensorIndex)p).ToArray();
+
+            // calculate the value at the given position
+            var value = CalculateAt(pos);
+            t[idx] = tensor(value);
+        }
+        return t;
+    }
+
+    /// <summary>
+    /// Calculates the value of the Levi-Civita tensor at a given position.
+    /// </summary>
+    public static long CalculateAt(params long[] a)
+    {
+        var productSum = 1L;
+        for (var i = 0; i < a.Length; i++)
+        for (var j = i + 1; j < a.Length; j++)
+        {
+            productSum *= Math.Sign(a[j] - a[i]);
+        }
+        return productSum;
+    }
+
+    /// <summary>
+    /// Calculates all permutations of the input including repetitions
+    /// </summary>
+    private static IEnumerable<IEnumerable<long>> CombinationsWithRepetition(long[] input, int length)
+    {
+        if (length <= 0)
+        {
+            yield break;
+        }
+
+        foreach (var i in input)
+        {
+            if (length - 1 <= 0)
+            {
+                yield return new long[] { i };
+            }
+
+            foreach (var c in CombinationsWithRepetition(input, length - 1))
+                yield return c.Append(i);
+        }
+    }
+}
